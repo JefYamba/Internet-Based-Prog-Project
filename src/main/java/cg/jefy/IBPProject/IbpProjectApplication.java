6576ef98d8cbc35219a404d4e81d1c44a454da41
JefYamba <118383030+JefYamba@users.noreply.github.com>
2024-02-29 11:36:15 +0300

    init commit

---
+++ src/main/java/cg/jefy/IBPProject/IbpProjectApplication.java
@@ -0,0 +1,13 @@
+package cg.jefy.IBPProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbpProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbpProjectApplication.class, args);
+	}
+
+}
